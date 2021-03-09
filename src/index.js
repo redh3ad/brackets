eae80543f6a9786e9680b604b900a51271647bf2
Egor Matyas <79442740+redh3ad@users.noreply.github.com>
2021-03-09 20:56:30 +0300

    feat
    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	var brc  = [];
+  
+	bracketsConfig.forEach(pr => {
+		brc.push(pr.join(''));
+	});
+	for(let i=0; i<brc.length; i++){
+	  if(str.includes(brc[i])){
+		str = str.replace(brc[i], "");
+		i = -1;
+	  }
+	}
+	return str.length === 0 ? true : false;
+  }
